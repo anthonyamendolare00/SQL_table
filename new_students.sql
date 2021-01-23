a2469638eaa258a95a6d81b2ffdb88142084d5d9
Anthony Amendolare <75578520+anthonyamendolare00@users.noreply.github.com>
2021-01-22 19:53:35 -0500

    Update new_students.sql

--- new_students.sql
+++ new_students.sql
@@ -10,13 +10,13 @@ CREATE TABLE new_students (
 );                        /* creates the table with columns */
 
 INSERT INTO new_students (first_name, last_name, grade, town, income)
-VALUES ('Frank', 'Hubert', 'Junior', 'Ronkonkoma', '30000'),
-	     ('Joseph', 'Allen', 'Senior', 'Oakdale', '120000'),
-	     ('Lucy', 'Mayfield', 'Sophomore', 'Ronkonkoma', '25000'),
-	     ('Julia', 'Jefferson', 'Freshmen', 'Bohemia', '70000'),
-	     ('Lucas', 'James', 'Sophomore', 'Oakdale', '150000'),
-	     ('John', 'Henry', 'Senior', 'Ronkonkoma', '15000'),
-	     ('Nicole', 'Hubert', 'Freshmen', 'Bohemia', '60000'),
-	     ('Anthony', 'Allen', 'Senior', 'Oakdale', '120000') /* inserts the values into the columns */
+VALUES  ('Frank', 'Hubert', 'Junior', 'Ronkonkoma', '30000'),
+	('Joseph', 'Allen', 'Senior', 'Oakdale', '120000'),
+	('Lucy', 'Mayfield', 'Sophomore', 'Ronkonkoma', '25000'),
+	('Julia', 'Jefferson', 'Freshmen', 'Bohemia', '70000'),
+	('Lucas', 'James', 'Sophomore', 'Oakdale', '150000'),
+	('John', 'Henry', 'Senior', 'Ronkonkoma', '15000'),
+	('Nicole', 'Hubert', 'Freshmen', 'Bohemia', '60000'),
+	('Anthony', 'Allen', 'Senior', 'Oakdale', '120000') /* inserts the values into the columns */
        
 SELECT new_students FROM students /* gets the table */
